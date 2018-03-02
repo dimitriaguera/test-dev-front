0edd98b5a1c75a2045e5009b003a9378645f3904
dimitriaguera <dimitri.aguera@gmail.com>
2018-03-02 21:08:19 +0100

    mise en place de la navigation

---
+++ app/scripts/menu.js
@@ -0,0 +1,21 @@
+function initMenu() {
+
+  const menuElmt = document.getElementById('mobile-menu');
+  const openElmt = document.getElementById('open-menu');
+  const closeElmt = document.getElementById('close-menu');
+
+  openElmt.addEventListener("click", openClickHandler, false);
+  closeElmt.addEventListener("click", closeClickHandler, false);
+
+  function openClickHandler(e) {
+    const classList = menuElmt.classList;
+    classList.add('is-open');
+  }
+
+  function closeClickHandler(e) {
+    const classList = menuElmt.classList;
+    classList.remove('is-open');
+  }
+}
+
+initMenu();
