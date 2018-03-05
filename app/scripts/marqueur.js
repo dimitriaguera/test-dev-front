48230b4b2b510d40f710a4752605012db7ee9ab2
dimitriaguera <dimitri.aguera@gmail.com>
2018-03-05 11:57:24 +0100

    responsive marqueur

--- app/scripts/marqueur.js
+++ app/scripts/marqueur.js
@@ -1,24 +1,27 @@
 const initMarqueur = function( Slider ) {
 
   const marqueurArray = document.getElementsByClassName('marqueur');
+  const listenersToRemove = [];
 
   for( var i = 0, l = marqueurArray.length; i < l; i++ ) {
     const mElmt = marqueurArray[i];
-    const closeElmt = mElmt.getElementsByClassName('close-menu-marqueur')[0];
+    //const closeElmt = mElmt.getElementsByClassName('close-menu-marqueur')[0];
     mElmt.addEventListener("click", openClickHandler.bind(mElmt), false);
-    closeElmt.addEventListener("click", closeClickHandler.bind(mElmt), false);
+    //closeElmt.addEventListener("click", closeClickHandler.bind(mElmt), false);
   }
 
   function openClickHandler(e) {
-    const classList = this.classList;
-    classList.add('is-open');
+    this.removeEventListener("click", openClickHandler, false);
+    this.addEventListener("click", closeClickHandler.bind(this), false);
+    this.classList.add('is-open');
     Slider.pause();
   }
 
   function closeClickHandler(e) {
     e.stopPropagation();
-    const classList = this.classList;
-    classList.remove('is-open');
+    this.removeEventListener("click", closeClickHandler, false);
+    this.addEventListener("click", openClickHandler.bind(this), false);
+    this.classList.remove('is-open');
     Slider.resume();
   }
 };
